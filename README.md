9db5b0243a35a914e0c6114cd0a58b0daed1696e
chayes <cfhayes16@gmail.com>
2023-07-17 21:03:01 -0600

    Got some basic movement in the car

---
+++ README.md
@@ -0,0 +1,3 @@
+# Self-Driving Car Simulation
+This project is from https://www.freecodecamp.org/news/self-driving-car-javascript/
+
--- car.js
+++ car.js
@@ -4,6 +4,7 @@ class Car {
         this.y = y;
         this.width = width;
         this.height = height;
+        this.controls = new Controls();
     }
 
     draw(ctx) {
@@ -15,5 +16,14 @@ class Car {
             this.height
         );
         ctx.fill();
+    }
+
+    update() {
+        if (this.controls.forward) {
+            this.y -= 2;
+        }
+        if (this.controls.reverse) {
+            this.y += 2;
+        }
     }
 }
