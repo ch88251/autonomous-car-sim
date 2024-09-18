c5da830f9c11431726bce645403f6250c16c9b35
Charles Hayes <cfhayes16@gmail.com>
2024-09-17 23:21:31 -0600

    Added sensor class

--- car.js
+++ car.js
@@ -4,23 +4,26 @@ class Car {
     this.y = y;
     this.width = width;
     this.height = height;
+
     this.speed = 0;
     this.acceleration = 0.2;
     this.maxSpeed = 3;
     this.friction = 0.05;
     this.angle = 0;
+
+    this.sensor = new Sensor(this);
     this.controls = new Controls();
   }
 
-  update() {
+  update(roadBorders) {
     this.#move();
+    this.sensor.update(roadBorders);
   }
 
   #move() {
     if (this.controls.forward) {
       this.speed += this.acceleration;
     }
-
     if (this.controls.reverse) {
       this.speed -= this.acceleration;
     }
@@ -28,7 +31,6 @@ class Car {
     if (this.speed > this.maxSpeed) {
       this.speed = this.maxSpeed;
     }
-
     if (this.speed < -this.maxSpeed / 2) {
       this.speed = -this.maxSpeed / 2;
     }
@@ -36,22 +38,18 @@ class Car {
     if (this.speed > 0) {
       this.speed -= this.friction;
     }
-
     if (this.speed < 0) {
       this.speed += this.friction;
     }
-
     if (Math.abs(this.speed) < this.friction) {
       this.speed = 0;
     }
 
     if (this.speed != 0) {
       const flip = this.speed > 0 ? 1 : -1;
-
       if (this.controls.left) {
         this.angle += 0.01 * flip;
       }
-
       if (this.controls.right) {
         this.angle -= 0.01 * flip;
       }
@@ -65,9 +63,13 @@ class Car {
     ctx.save();
     ctx.translate(this.x, this.y);
     ctx.rotate(-this.angle);
+
     ctx.beginPath();
     ctx.rect(-this.width / 2, -this.height / 2, this.width, this.height);
     ctx.fill();
+
     ctx.restore();
+
+    this.sensor.draw(ctx);
   }
 }
